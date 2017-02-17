088bbb8194b17a57230cbb6a58a5ddbc80013c2b
lelinh <lemanhlinhbk@gmail.com>
2017-02-17 22:55:01 +0700

    add pull to refresh

--- MovieDB/MovieListViewController.swift
+++ MovieDB/MovieListViewController.swift
@@ -15,38 +15,21 @@ class MovieListViewController: UIViewController, UITableViewDelegate,UITableView
     
     var movie = [NSDictionary]()
     let tmdbUrl = "https://image.tmdb.org/t/p/w500"
-    
+    // Initialize a UIRefreshControl
+    let refreshController = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        loadMovieDataBase()
         
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
-        let request = URLRequest(
-            url: url!,
-            cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
-            timeoutInterval: 10)
-        let session = URLSession(
-            configuration: URLSessionConfiguration.default,
-            delegate: nil,
-            delegateQueue: OperationQueue.main
-        )
-        let task: URLSessionDataTask =
-            session.dataTask(with: request,
-                             completionHandler: { (dataOrNil, response, error) in
-                                if let data = dataOrNil {
-                                    if let responseDictionary = try! JSONSerialization.jsonObject(
-                                        with: data, options:[]) as? NSDictionary {
-                                        self.movie = (responseDictionary["results"] as! [NSDictionary])
-                                        self.tableView.reloadData()
-                                    }
-                                }
-            })
-        task.resume()
+        //Add refresh database
+        refreshController.addTarget(self, action: #selector(refreshControlAction(refreshController:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshController, at: 0)
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,8 +55,40 @@ class MovieListViewController: UIViewController, UITableViewDelegate,UITableView
         print(imageUrl)
         return cell
     }
-
-    /*
+    
+    func loadMovieDataBase() {
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = URLRequest(
+            url: url!,
+            cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate: nil,
+            delegateQueue: OperationQueue.main
+        )
+        let task: URLSessionDataTask =
+            session.dataTask(with: request,
+                             completionHandler: { (dataOrNil, response, error) in
+                                if let data = dataOrNil {
+                                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                                        with: data, options:[]) as? NSDictionary {
+                                        self.movie = (responseDictionary["results"] as! [NSDictionary])
+                                        self.tableView.reloadData()
+                                        //end refresh database
+                                        self.refreshController.endRefreshing()
+                                    }
+                                }
+            })
+        task.resume()
+    }
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    func refreshControlAction(refreshController: UIRefreshControl) {
+        loadMovieDataBase()
+    }
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -81,6 +96,6 @@ class MovieListViewController: UIViewController, UITableViewDelegate,UITableView
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+ 
 
 }
